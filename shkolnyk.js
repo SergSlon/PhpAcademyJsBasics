4572530595066b1997e3d26709da6a514be5cdac
Anton Shkolnyk <shkolnyk@ukr.net>
2016-08-19 14:02:42 +0300

    Task 1 and 2

--- shkolnyk.js
+++ shkolnyk.js
@@ -1,70 +1,53 @@
 ﻿// First Task
-var travelCalc = function(alreadyCompleted, distance, kmPerDay) {
+
+var getTravelTime = function(alreadyCompleted, distance, kmPerDay) {
     if (distance <= 0 || kmPerDay <= 0) {
-        alert("Error");
+        try {
+            throw new Error("Error");
+        } catch(err) {
+            alert("Error");
+        }
     } else {
         var allTime = (distance - alreadyCompleted) / kmPerDay;
         var days = Math.floor(allTime);
         var hours = Math.floor(((allTime%1)*24));
         var minutes = ((((allTime%1)*24)%1)*60).toFixed(0);
 
-        return [days, hours, minutes];
+        return {
+            "Days": days,
+            "Hours": hours,
+            "Minutes": minutes
+        };
+
     }
+
+};
+
+function getTravelTimeText(obj) {
+    var text = {
+        day: "day",
+        hour: "hour",
+        minute: "minute"
+    };
+
+    if (obj["Days"] !== 1) {
+        text.day += "s";
+    }
+    if (obj["Hours"] !== 1) {
+        text.hour += "s";
+    }
+    if (obj["Minutes"] !== 1) {
+        text.minute += "s";
+    }
+
+    return("The road will take: " + obj["Days"] + " " + text.day + " " + obj["Hours"] + " " + text.hour + " " + obj["Minutes"] + " " + text.minute);
 
 }
 
-var newTravel = travelCalc(50,225,45);
+var travelDistanceTime = getTravelTime(80,101,20);
+var travelTimeText = getTravelTimeText(travelDistanceTime);
+console.log(travelTimeText);
 
-
-var textEdit = (function () {
-    var daysText = "";
-    var hoursText = "";
-    var minutesText = "";
-    if (newTravel[0] == 1) {
-        daysText = "day";
-    } else {
-        daysText = "days";
-    }
-
-    if (newTravel[1] == 1) {
-        hoursText = "hour";
-    } else {
-        hoursText = "hours";
-    }
-
-    if (newTravel[2] == 1) {
-        minutesText = "minute";
-    } else {
-        minutesText = "minutes";
-    }
-
-    if(newTravel[2] == 0 && newTravel[1] == 0) {
-        console.log("The road will take: " + newTravel[0] + " " + daysText);
-
-        return;
-    } if(newTravel[1] == 0 && newTravel[0] == 0) {
-        console.log("The road will take: " + newTravel[2] + " " + minutesText);
-
-        return;
-    } if (newTravel[0] == 0 && newTravel[2] == 0) {
-        console.log("The road will take: "  + newTravel[1] + " " + hoursText);
-
-        return;
-    } if (newTravel[0] == 0) {
-        console.log("The road will take: " + newTravel[1] + " " + hoursText + ", " + newTravel[2] + " " + minutesText);
-
-        return;
-    } if (newTravel[1] == 0) {
-        console.log("The road will take: " + newTravel[0] + " " + daysText + ", "  + newTravel[2] + " " + minutesText);
-
-        return;
-    } if (newTravel[2] == 0) {
-        console.log("The road will take: " + newTravel[0] + " " + daysText + ", " + newTravel[1] + " " + hoursText);
-    } else {
-        console.log("The road will take: " + newTravel[0] + " " + daysText + ", " + newTravel[1] + " " + hoursText + ", " + newTravel[2] + " " + minutesText);
-    }
-
-})();
 
 // Second Task
 
@@ -84,22 +67,36 @@ var textEdit = (function () {
  * @return {Object}
  */
 
-
-function Event(name, start, end, members, address) {
+function validateEvent(name, start, end, members) {
     if (new Date() > start || new Date() > end || end < start){
-        alert("Invalid Date");
-    } else if(typeof(name) == "string" && typeof(start) == "object" && typeof(end) == "object" && typeof(members) == "object" && typeof(address) == "string") {
-        return {
-            "Name": name,
-            "Start Date": start,
-            "End Date": end,
-            "Members": members,
-            "address": address
-        };
+        try {
+            throw new Error("Error");
+        } catch(err) {
+            alert("Error");
+        }
+    } else if(Object.prototype.toString.call(name) == "[object String]" && Object.prototype.toString.call(start) == "[object Date]" && Object.prototype.toString.call(end) == "[object Date]" && Object.prototype.toString.call(members) == "[object Array]") {
+
+        return [name, start, end, members];
     } else {
-        alert("Error");
+        try {
+            throw new Error("Error");
+        } catch(err) {
+            alert("Error");
+        }
     }
 }
-var newEvent = Event("Party", new Date('2016-08-22T20:00:00'), new Date('2016-08-23T06:30:00'), ["Andrey", "Ivan", "Nasty"], "Shevchenko str 35 kv 48");
+
+function Event(data) {
+    return {
+        "Name": data[0],
+        "Start Date": data[1],
+        "End Date": data[2],
+        "Members": data[3]
+    };
+}
+
+
+var newEventValidation = validateEvent("Party", new Date('2016-08-22T20:00:00'), new Date('2016-08-23T06:30:00'), ["Andrey", "Ivan", "Nasty"]);
+var newEvent = new Event(newEventValidation);
 console.log(newEvent);
 
