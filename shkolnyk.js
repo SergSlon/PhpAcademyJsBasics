14cedc725be62b2f402bae96c22295d5ea87e785
Anton Shkolnyk <shkolnyk@ukr.net>
2016-10-06 19:57:08 +0300

    DZ Prototype

--- shkolnyk.js
+++ shkolnyk.js
@@ -1,21 +1,48 @@
-window.onload = function () {
-	var products = document.getElementsByClassName("products");
-	var discription = document.getElementsByClassName("discription");
-	var mainImg = document.getElementsByTagName("img");
-				
-	for(var i=0; i<products.length; i++)	{
-		var htmlLoop = function  (i) {
-			products[i].onmouseover = function() {
-				var xhr = new XMLHttpRequest();
-				mainImg[i].style.display = "";
-				xhr.open("GET", "http://www.randomtext.me/api/lorem/h2/2-2/", false);
-				xhr.send();
-				var outText = JSON.parse(xhr.responseText);			
-				discription[i].innerHTML = outText.text_out;
-			}
-		}
-		htmlLoop(i);
-	}
-}
+var Figure = function(color) {
+    this.color = color || "black";
+};
 
 
+var Square = function(width, height, color) {
+    Figure.call(this, color);
+    this.width = width;
+    this.height = height;
+    this.area = function() {
+        return this.width * this.height;
+    }
+};
+
+Square.prototype = Object.create(Figure.prototype);
+Square.prototype.constructor = Square;
+
+var Rectangle = function(width, height, color) {
+    Figure.call(this, color);
+    this.width = width;
+    this.height = height;
+    this.area = function() {
+        return this.width * this.height;
+    }
+};
+
+Rectangle.prototype = Object.create(Figure.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+var Circle = function(radius, color) {
+    Figure.call(this, color);
+    this.radius = radius;
+    this.area = function() {
+        return 3.14 * (this.radius * this.radius);
+    }
+};
+
+var square = new Square (20, 30, "green");
+var rectangle = new Rectangle(13, 4, "blue");
+var circle = new Circle(13, "red");
+
+
+console.log(square);
+console.log("Area:" + square.area());
+console.log(rectangle);
+console.log("Area:" + rectangle.area());
+console.log(circle);
+console.log("Area:" + circle.area());
